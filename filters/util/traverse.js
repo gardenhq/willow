4006b2326da566c7e0a427f0bb39b30a2909e0a2
gardener <gardener@gardenhq.io>
2017-06-07 14:53:10 +0100

    Fix: IE doesn't like Object.getPrototypeOf("string")
    IE11 at least doesn't like passing a string to gPO. Check for a string
    before passing through.

--- filters/util/traverse.js
+++ filters/util/traverse.js
@@ -1,13 +1,12 @@
 module.exports = function traverse(obj, callback, trail)
 {
     trail = trail || []
-
     Object.keys(obj).forEach(
-        function (key)
+        function(key)
         {
-            var value = obj[key]
-
-            if (value != null && Object.getPrototypeOf(value) === Object.prototype) {
+            var value = obj[key];
+            // IE doesn't like Object.getPrototypeOf("string")
+            if (value != null && typeof value === "object" && Object.getPrototypeOf(value) === Object.prototype) {
                 traverse(value, callback, trail.concat(key))
             } else {
                 obj[key] = callback.call(obj, value, key, trail)
